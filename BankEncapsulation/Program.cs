87afb253568a526b86051ad5e6b132ad1b08d76d
Sam Booth <ballersabooth@yahoo.com>
2020-09-02 11:58:34 -0500

    commited changes

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -6,7 +6,17 @@ namespace BankEncapsulation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAccount ba = new BankAccount();
+
+            Console.WriteLine("How much $$$ do you want to deposit?");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            ba.Deposit(amountToDeposit);
+           
+            double userBalance = ba.GetBalance();
+
+            Console.WriteLine("Your current balance is: {userBalance, 0:c}");
+
         }
     }
 }
